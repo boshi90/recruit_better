96d94eef1bf90947638c25f1e7c60ce66be4a4f0
Raghu Betina <raghu@firstdraft.com>
2018-03-15 13:06:45 +0000

    Only target user can modify target

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -1,4 +1,14 @@
 class TargetsController < ApplicationController
+  before_action :current_user_must_be_target_user, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_target_user
+    target = Target.find(params[:id])
+
+    unless current_user == target.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @targets = Target.all
 
