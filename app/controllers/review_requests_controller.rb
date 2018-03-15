83c091d722ef696b89b92b068e4f4966b461f660
Raghu Betina <raghu@firstdraft.com>
2018-03-15 13:06:45 +0000

    Only review_request user can modify review_request

--- app/controllers/review_requests_controller.rb
+++ app/controllers/review_requests_controller.rb
@@ -1,4 +1,14 @@
 class ReviewRequestsController < ApplicationController
+  before_action :current_user_must_be_review_request_applicant, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_review_request_applicant
+    review_request = ReviewRequest.find(params[:id])
+
+    unless current_user == review_request.applicant
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @review_requests = ReviewRequest.all
 
